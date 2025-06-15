c110b4a17ea48888c54cf1e45b49cb7c45a2e939
germans <grajevskis@gmail.com>
2025-06-15 16:23:59 +0300

    Fixed Prettier function.

--- tests/homework_09_06-api.spec.ts
+++ tests/homework_09_06-api.spec.ts
@@ -12,17 +12,17 @@ test('put order with correct data and ID 1  should recieve 200 OK', async ({ req
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/1', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
@@ -34,17 +34,17 @@ test('put order with correct data and ID 10 should recieve 200 OK', async ({ req
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/10', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
@@ -55,17 +55,17 @@ test('put order with 0 Id should return 400 BAD Request', async ({ request }) =>
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/0', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
@@ -76,17 +76,17 @@ test('put order with missing ID  should return 400 BAD Request', async ({ reques
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
 })
 
@@ -98,17 +98,17 @@ test('put order with 11 Id should return 400 BAD Request', async ({ request }) =
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/11', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
@@ -120,17 +120,17 @@ test('put order with 5 Id should return 200 BAD OK', async ({ request }) => {
     customerName: 'adrian',
     customerPhone: '+371222222',
     comment: 'hello world',
-    id: 1
+    id: 1,
   }
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/5', {
     data: requestBody,
     headers: requestHeaders,
-  });
-  console.log('response status:', response.status());
-  console.log('response body:', await response.json());
+  })
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
@@ -138,8 +138,8 @@ test('put order with 5 Id should return 200 BAD OK', async ({ request }) => {
 
 test('delete order with 1 ID  should receive code 204', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/1', {
     headers: requestHeaders,
   })
@@ -148,8 +148,8 @@ test('delete order with 1 ID  should receive code 204', async ({ request }) => {
 
 test('delete order with 10 ID  should receive code 204', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/10', {
     headers: requestHeaders,
   })
@@ -158,8 +158,8 @@ test('delete order with 10 ID  should receive code 204', async ({ request }) => 
 
 test('delete order with 5 ID  should receive code 204', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
     headers: requestHeaders,
   })
@@ -168,19 +168,18 @@ test('delete order with 5 ID  should receive code 204', async ({ request }) => {
 
 test('delete order with 0 ID  should receive code 400', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/0', {
     headers: requestHeaders,
   })
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 
-
 test('delete order with 11 ID  should receive code 400', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/11', {
     headers: requestHeaders,
   })
@@ -189,8 +188,8 @@ test('delete order with 11 ID  should receive code 400', async ({ request }) => 
 
 test('delete order with no ID  should receive code 405', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/', {
     headers: requestHeaders,
   })
@@ -199,8 +198,8 @@ test('delete order with no ID  should receive code 405', async ({ request }) => 
 
 test('delete order with ID = String  should receive code 400', async ({ request }) => {
   const requestHeaders = {
-    'api_key' : '1234567891234561'
-  };
+    api_key: '1234567891234561',
+  }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/aa', {
     headers: requestHeaders,
   })
@@ -209,18 +208,26 @@ test('delete order with ID = String  should receive code 400', async ({ request 
 
 // GET
 
-test('Get order with correct parameters username and password - answer 200 OK', async ({ request }) => {
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders?username=Germans&password=asd')
+test('Get order with correct parameters username and password - answer 200 OK', async ({
+  request,
+}) => {
+  const response = await request.get(
+    'https://backend.tallinn-learning.ee/test-orders?username=Germans&password=asd',
+  )
   expect(response.status()).toBe(StatusCodes.OK)
 })
 
-test('Get order with no parameters username and password - Internal Server Errorr ', async ({ request }) => {
+test('Get order with no parameters username and password - Internal Server Errorr ', async ({
+  request,
+}) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders')
   expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
 })
 
 test('Get order with only 1 parameter  username - Internal Server Errorr ', async ({ request }) => {
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders?username=Germans')
+  const response = await request.get(
+    'https://backend.tallinn-learning.ee/test-orders?username=Germans',
+  )
   expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
 })
 
@@ -228,6 +235,3 @@ test('Get order with only 1 parameter password - Internal Server Errorr ', async
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders?password=asd')
   expect(response.status()).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
 })
-
-
-
